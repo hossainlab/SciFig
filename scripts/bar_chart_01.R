5cd2aba08c36ddaa71c49c79d086104d7c7e5cfd
Jubayer Hossain <work.jubayer@gmail.com>
2022-01-03 22:09:43 +0600

    intial commit

---
+++ scripts/bar_chart_01.R
@@ -0,0 +1,13 @@
+library(tidyverse) # for summarizing the data
+library(ggsci) # for color palettes of scientific journals
+library(ggpubr) # for publication ready themes 
+
+
+
+
+
+
+
+
+
+ggsave("test.tiff", units="in", width=12, height=6, dpi=300, compression = 'lzw')
