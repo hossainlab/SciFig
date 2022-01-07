d9f0d753ba3e834f8f8231cb6dcbbf621762a83a
Jubayer Hossain <work.jubayer@gmail.com>
2022-01-08 00:27:35 +0600

    added code

--- scripts/Day_01_Bar_Charts.R
+++ scripts/Day_01_Bar_Charts.R
@@ -1,13 +1,18 @@
+# Load required packages 
 library(tidyverse) # for summarizing the data
 library(ggsci) # for color palettes of scientific journals
 library(ggpubr) # for publication ready themes 
+library(gapminder)
 
+# Single Categorical Variable Visualization
+gapminder %>% 
+  drop_na(continent) %>% 
+  ggplot(aes(x = fct_infreq(continent)))+
+  geom_bar(fill = "#97B3C6")+
+  labs(title = "Number of observations per continent", 
+       x = "Continent", 
+       y = "Counts")+
+  theme_pubr()
 
-
-
-
-
-
-
-
-ggsave("test.tiff", units="in", width=12, height=6, dpi=300, compression = 'lzw')
+# Save Figure 
+ggsave("outputs/test.tiff", units="in", width=12, height=6, dpi=300, compression = 'lzw')
